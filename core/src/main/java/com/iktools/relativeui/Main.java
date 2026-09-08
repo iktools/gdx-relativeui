18dfbd54425babf4b9257fb4167def6727250397
Ivan Kravarščan <subchannel13@gmail.com>
2026-09-08 20:58:10 +0200

    Added basic Scene2d UI

--- core/src/main/java/com/iktools/relativeui/Main.java
+++ core/src/main/java/com/iktools/relativeui/Main.java
@@ -1,32 +1,45 @@
 package com.iktools.relativeui;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    private Stage stage;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        stage = new Stage(new ScreenViewport());
+
+        Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        Table rootTable = new Table();
+        rootTable.setFillParent(true);
+        rootTable.add(new Label("Column 1", skin));
+
+        stage.addActor(rootTable);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        stage.dispose();
+    }
+
+    @Override
+    public void resize (int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 }
